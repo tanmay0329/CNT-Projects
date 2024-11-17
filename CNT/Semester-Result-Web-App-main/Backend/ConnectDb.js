7bcc9a5aa0118ddfd4745ac25b478ae37504e08a
Tanmay Bora <139738349+tanmay0329@users.noreply.github.com>
2024-11-17 22:12:42 +0530

    Delete CNT/Semester-Result-Web-App-main directory

--- CNT/Semester-Result-Web-App-main/Backend/ConnectDb.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectToDB(url){
-    return mongoose.connect(url);
-}
-
-module.exports = {connectToDB}
--- CNT/Semester-Result-Web-App-main/Backend/Controller/SemesterResult.js
+++
@@ -1,9 +0,0 @@
-const {semResult} = require("../Model/SemesterData");
-
-async function AddNewResult(data) {
-    const reuslt =  await semResult.create(data);
-}
-
-module.exports = {
-    AddNewResult,
-}
